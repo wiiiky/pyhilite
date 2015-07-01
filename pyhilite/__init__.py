0b0590c763e75f4c21800f9d9c5ba17bb2247894
wiiiky <wiiiky@outlook.com>
2015-07-01 17:14:50 +0800

    配置setuptools

--- pyhilite/__init__.py
+++ pyhilite/__init__.py
@@ -1,1 +1,12 @@
 # encoding=utf8
+
+from . import srchilite
+import os
+
+def SourceHighlight(**kargs):
+    if 'datadir' not in kargs:
+        kargs['datadir'] = os.path.join(os.path.dirname(__file__), 'data')
+    return srchilite.SourceHighlight(**kargs)
+
+
+__all__ = []
--- setup.py
+++ setup.py
@@ -2,25 +2,37 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup, find_packages, Extension
 except:
-    from distutils.core import setup, find_packages
+    from distutils.core import setup, find_packages, Extension
+
+
+srchilite = Extension('pyhilite.srchilite',
+                      libraries=['source-highlight'],
+                      sources=['pyhilite/srchilite.cc'])
 
 setup(
     name='pyhilite',
-    version='0.0.1',
+    version='0.1.0',
     description='Short description',
     long_description=''.join(open('README').readlines()),
-    keywords='some, keywords',
-    author='yourname',
-    author_email='yourmail',
+    keywords='Source Code, Highlight, HTML, Latex',
+    author='Wiky L',
+    author_email='wiiiky@outlook.com',
     license='GPLv3',
-    packages=find_packages(),
+    packages=['pyhilite'],
+    package_dir={
+        'pyhilite': 'pyhilite',
+    },
+    package_data={
+        'pyhilite': ['data/*']
+    },
+    ext_modules=[srchilite],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        ]
+    ]
 )
