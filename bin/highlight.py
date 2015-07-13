0d39dcc73e7b746d7cab1d7c1328eef587b01811
wiiiky <wiiiky@outlook.com>
2015-07-13 11:24:08 +0800

    highlite.py添加行号支持

--- bin/highlight.py
+++ bin/highlight.py
@@ -4,16 +4,19 @@ import argparse
 import sys
 import pyhilite
 
-p = argparse.ArgumentParser(add_help=False)
-p.add_argument('-i', '--input', default='STDIN', help='源代码文件，未指定为默认标准输入')
-p.add_argument('-o', '--output', default='STDOUT', help='格式化代码输出文件，未指定为标准输出')
-p.add_argument('-l', '--lang', default='', help='源代码的语言，标准输入必须指定，默认python')
-p.add_argument('-O', '--outlang', default='html', help='输出格式，默认为HTML格式')
-p.add_argument('-h', '--help', action='help', help='显示此帮助信息')
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('-i', '--input', default='STDIN', help='源代码文件，未指定为默认标准输入')
+parser.add_argument('-o', '--output', default='STDOUT', help='格式化代码输出文件，未指定为标准输出')
+parser.add_argument('-l', '--lang', default='', help='源代码的语言，标准输入必须指定，默认python')
+parser.add_argument('-O', '--outlang', default='html', help='输出格式，默认为HTML格式')
+parser.add_argument('-n', '--number', action='store_true', help='显示行号')
+parser.add_argument('-h', '--help', action='help', help='显示此帮助信息')
 
-args = p.parse_args()
+args = parser.parse_args()
 
 hilite = pyhilite.SourceHighlight(outlang=args.outlang)
+if args.number:
+    hilite.setGenerateLineNumbers(True)
 
 if args.input == 'STDIN' and args.lang != '':
     formatted = hilite.highlight(data=sys.stdin.read(), lang=args.lang)
@@ -23,11 +26,10 @@ elif args.input != 'STDIN':
     else:
         formatted = hilite.highlight(input=args.input, lang=args.lang)
 else:
-    p.print_help()
-    p.exit()
+    parser.print_help()
+    parser.exit()
 
 if args.output == 'STDOUT':
     print(formatted)
 else:
     open(args.output, 'w').write(formatted)
-
